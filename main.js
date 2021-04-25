daa0c30f73bddcc6a25b75231c3a97dc8200b825
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-25 12:25:13 +0100

    feat: issue open/close notifications

--- main.js
+++ main.js
@@ -42,8 +42,56 @@ const EVENTS = [
 
 exports.rule = entities.Issue.onChange({
     title: "Discord Webhook",
+    guard: (ctx) => {
+        return ctx.issue.isReported;
+    },
     action: (ctx) => {
         const issue = ctx.issue;
+
+        if (issue.becomesReported) {
+            const payload = new Payload(null, CONFIG.SENDER_NAME, CONFIG.AVATAR_URL);
+            const embed = new Embed();
+            const body = new Body();
+
+            body.title = "Issue " + issue.id + " Created";
+            body.description = issue.description;
+            body.url = issue.url;
+            body.color = CONFIG.COLOR_NEGATIVE;
+            body.setDateToNow();
+            embed.body = body;
+
+            const user = ctx.currentUser;
+            embed.author = new Author(user.visibleName, CONFIG.YOUTRACK_URL + "/users/" + user.login, user.avatarUrl);
+
+            embed.footer = new Footer(CONFIG.SITE_NAME + " " + issue.project.name);
+
+            payload.addEmbed(embed);
+            payload.send(CONFIG.WEBHOOK_URL);
+
+            return;
+        }
+        else if (issue.becomesResolved) {
+            const payload = new Payload(null, CONFIG.SENDER_NAME, CONFIG.AVATAR_URL);
+            const embed = new Embed();
+            const body = new Body();
+
+            body.title = "Issue " + issue.id + " Resolved";
+            body.description = issue.description;
+            body.url = issue.url;
+            body.color = CONFIG.COLOR_POSITIVE;
+            body.setDateToNow();
+            embed.body = body;
+
+            const user = ctx.currentUser;
+            embed.author = new Author(user.visibleName, CONFIG.YOUTRACK_URL + "/users/" + user.login, user.avatarUrl);
+
+            embed.footer = new Footer(CONFIG.SITE_NAME + " " + issue.project.name);
+
+            payload.addEmbed(embed);
+            payload.send(CONFIG.WEBHOOK_URL);
+
+            return;
+        }
         
         let changes = [];
         for (let i = 0; i < EVENTS.length; i++) {
